9aab462a2db45955d1ec59bfd7ea98f94ac66454
flipflop133 <francois.bechet@tutanota.com>
2025-01-07 10:47:17 +0100

    add common cmake

---
+++ common.cmake
@@ -0,0 +1,34 @@
+# Set common project settings
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_LINKER /usr/bin/ld.lld)
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+
+set(CMAKE_C_STANDARD 23)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS OFF)
+
+# Add common include directories
+include_directories(
+    /usr/include
+    /usr/include/freetype2/
+)
+
+# Add common build type options
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type" FORCE)
+endif()
+
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    add_compile_options(-g -Wall -Wextra)
+elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+    add_compile_options(-O3 -flto -march=native -mtune=native)
+endif()
+
+# Add common status messages
+message(STATUS "CMake version: ${CMAKE_VERSION}")
+message(STATUS "CMake system name: ${CMAKE_SYSTEM_NAME}")
+message(STATUS "CMake system version: ${CMAKE_SYSTEM_VERSION}")
+message(STATUS "CMake C compiler: ${CMAKE_C_COMPILER}")
+message(STATUS "CMake CXX compiler: ${CMAKE_CXX_COMPILER}")
+message(STATUS "Linker: ${CMAKE_LINKER}")
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
